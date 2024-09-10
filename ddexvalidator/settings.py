bc2bba7c5251ec2382b4d03bd1be10765cff1b9b
Mariusz Smenżyk <mariusz@bravelab.io>
2024-09-11 00:46:58 +0200

    Add env conf

--- ddexvalidator/settings.py
+++ ddexvalidator/settings.py
@@ -8,17 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--l1nq68qzjb!m4mljp$^tp9zf(m^c(e3ee^3a-=n02s3k$&a$u"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'vdm30df2-s=2fdkskd.s-23-a_sdfm1')
+DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
