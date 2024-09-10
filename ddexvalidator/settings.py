bcfd699643ae5330ad41c1cc413c2320d2550daa
Mariusz Smenżyk <mariusz@bravelab.io>
2024-09-11 01:55:05 +0200

    Allowed host

--- ddexvalidator/settings.py
+++ ddexvalidator/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'vdm30df2-s=2fdkskd.s-23-a_sdfm1')
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 INSTALLED_APPS = [
